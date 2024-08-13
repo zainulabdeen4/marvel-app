e9aac87bb0568391836062d98a9cd846c9cac719
EMKC0278 <EMKC0278@emkanfinance.com.sa>
2024-08-13 16:09:28 +0300

    unused import removed

--- src/translations/i18n.ts
+++ src/translations/i18n.ts
@@ -2,7 +2,6 @@ import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 import en from './en.json';
 import ar from './ar.json';
-import * as RNLocalize from 'react-native-localize';
 i18n.use(initReactI18next).init({
   // lng: RNLocalize.getLocales()[0].languageTag,
   compatibilityJSON: 'v3',
